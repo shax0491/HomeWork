f86610d55f77d111eaf1e845ed93d912ba2be49d
AndreyT <shax0491@gmail.com>
2023-09-20 11:35:06 +0300

    Выполнение ДЗ

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("НЕТ!");
+}
---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int even = 2;
+
+while (num >= even)
+{
+    Console.Write(even + " ");
+    even = even + 2;
+}
